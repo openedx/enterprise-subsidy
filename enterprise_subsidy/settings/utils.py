86f42a5bd137596eeb5ff77fe8e83967f5b7fe1e
John Nagro <jnagro@edx.org>
2023-05-15 14:27:13 +0000

    feat: enable x-request-id logging, replying

--- enterprise_subsidy/settings/utils.py
+++ enterprise_subsidy/settings/utils.py
@@ -40,7 +40,8 @@ def get_logger_config(logging_env="no_env",
         'formatters': {
             'standard': {
                 'format': '%(asctime)s %(levelname)s %(process)d '
-                          '[%(name)s] [user %(userid)s] [ip %(remoteip)s] %(filename)s:%(lineno)d - %(message)s',
+                          '[%(name)s] [user %(userid)s] [ip %(remoteip)s] '
+                          '[request_id %(request_id)s] %(filename)s:%(lineno)d - %(message)s',
             },
             'syslog_format': {'format': syslog_format},
             'raw': {'format': '%(message)s'},
@@ -54,6 +55,9 @@ def get_logger_config(logging_env="no_env",
             },
             'remoteip_context': {
                 '()': 'edx_django_utils.logging.RemoteIpFilter',
+            },
+            'request_id': {
+                '()': 'log_request_id.filters.RequestIDFilter'
             }
         },
         'handlers': {
@@ -61,7 +65,7 @@ def get_logger_config(logging_env="no_env",
                 'level': 'DEBUG' if debug else 'INFO',
                 'class': 'logging.StreamHandler',
                 'formatter': 'standard',
-                'filters': ['userid_context', 'remoteip_context'],
+                'filters': ['userid_context', 'remoteip_context', 'request_id'],
                 'stream': sys.stdout,
             },
         },
