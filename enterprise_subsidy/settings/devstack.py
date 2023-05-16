0830cad0aed82d8608f2a62408e963451f07e6a1
Emily Aquin <eaquin@2u.com>
2023-05-16 16:39:48 +0000

    feat: add localhost for frontend-app-learner-portal-enterprise to enterprise-subsidy

--- enterprise_subsidy/settings/devstack.py
+++ enterprise_subsidy/settings/devstack.py
@@ -2,6 +2,7 @@ from enterprise_subsidy.settings.local import *
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:18450',  # frontend-app-support-tools
+    'http://localhost:8734',  # frontend-app-learner-portal-enterprise
 )
 
 DATABASES = {
