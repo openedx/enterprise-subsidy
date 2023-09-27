3c0eb54ef3c947bb673eaed352bb7510d110ea3e
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-09-27 19:51:05 +0500

    fix: import order fixed

--- enterprise_subsidy/apps/api/urls.py
+++ enterprise_subsidy/apps/api/urls.py
@@ -4,8 +4,7 @@ Root API URLs.
 All API URLs should be versioned, so urlpatterns should only
 contain namespaces for the active versions of the API.
 """
-from django.urls import path
-from django.urls import include
+from django.urls import include, path
 
 from enterprise_subsidy.apps.api.v1 import urls as v1_urls
 from enterprise_subsidy.apps.api.v2 import urls as v2_urls
